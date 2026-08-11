741e0c6616d7914e2e9ba483680e7a2e525e3129
MmError404 <hajarbenzakour6@gmail.com>
2026-08-11 15:49:53 +0100

    Ajout de la route metrics pour Prometheus

--- app/app.py
+++ app/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template_string, jsonify
+from flask import Flask, render_template_string, jsonify, Response, request
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    "pfa_http_requests_total",
+    "Nombre total de requêtes HTTP reçues par l'application",
+    ["method", "endpoint", "http_status"]
+)
 
 tickets = [
     {"id": 1, "titre": "Problème réseau", "statut": "Ouvert"},
     {"id": 2, "titre": "Demande accès application", "statut": "En cours"},
     {"id": 3, "titre": "Erreur serveur", "statut": "Résolu"},
 ]
+
+
+@app.after_request
+def count_requests(response):
+    REQUEST_COUNT.labels(
+        method=request.method,
+        endpoint=request.path,
+        http_status=response.status_code
+    ).inc()
+    return response
+
 
 @app.route("/")
 def home():
@@ -55,7 +73,7 @@ def home():
     <body>
         <div class="container">
             <h1>Application de gestion des tickets</h1>
-            <p>Projet PFA : Chaîne DevOps Cloud-Native avec CI/CD, Kubernetes et GitOps.</p>
+            <p>Projet PFA : Chaîne DevOps Cloud-Native avec CI/CD, Kubernetes, GitOps et Monitoring.</p>
 
             <table>
                 <tr>
@@ -77,6 +95,7 @@ def home():
     """
     return render_template_string(html, tickets=tickets)
 
+
 @app.route("/health")
 def health():
     return jsonify({
@@ -84,6 +103,12 @@ def health():
         "service": "pfa-ticket-app",
         "version": "1.0.0"
     }), 200
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
